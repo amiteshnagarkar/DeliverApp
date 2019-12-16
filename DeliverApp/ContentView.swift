e9dce2990f4f3484273a33d5371bb57409e6b469
amiteshnagarkar <amiteshnagarkar@gmail.com>
2019-12-16 18:52:25 +0000

    created xcode project

---
+++ DeliverApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DeliverApp
+//
+//  Created by Amitesh Nagarkar on 16/12/2019.
+//  Copyright © 2019 Amitesh Nagarkar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
